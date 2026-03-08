56b9de88aeac9658c2847c7228b496810942a2b8
Travis Dunbar <travis.j.dunbar@gmail.com>
2026-03-08 09:41:39 -0500

    goose and sqlc

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/tbone317/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int64
+	db             *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+	log.Printf("Using DB_URL: %s", dbURL)
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+	defer dbConn.Close()
+
 	mux := http.NewServeMux()
-	cfg := &apiConfig{}
+	cfg := &apiConfig{
+		fileserverHits: atomic.Int64{},
+		db:             database.New(dbConn),
+	}
 	fsHandler := cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	mux.Handle("/app/", fsHandler)
 
